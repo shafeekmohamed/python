2f3a9790daaa49558602289da4bc861842aa66bf
anazanz <anazm52@gmail.com>
2020-12-14 12:22:11 +0530

    storing ti github

---
+++ fntn.py
@@ -0,0 +1,9 @@
+para="this is my sample program and easy to use"
+unique_list=[]
+for a in para:
+    if a not in unique_list:
+        unique_list.append(a)
+
+print(unique_list)
+
+
---
+++ sort.py
@@ -0,0 +1,4 @@
+item=['1','anaz',9297701851]
+print("name:" + item[1] + ",","id:" +item[0],",","number:",item[2])
+
+
